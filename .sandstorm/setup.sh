46191a767c2beae7a205f2a7570c97006059bda3
Michael Nutt <michael@nuttnet.net>
2020-03-01 12:06:20 -0500

    add capnp for generating queries

--- .sandstorm/setup.sh
+++ .sandstorm/setup.sh
@@ -17,7 +17,7 @@ echo "Installing the NodeSource Node.js 10.x repo..."
 
 curl -sL https://deb.nodesource.com/setup_10.x | bash -
 
-apt-get install -y -qq --no-install-suggests --no-install-recommends nodejs git-core g++ libreoffice-writer libreoffice-impress libreoffice-calc unoconv
+apt-get install -y -qq --no-install-suggests --no-install-recommends nodejs git-core g++ libreoffice-writer libreoffice-impress libreoffice-calc unoconv clang autoconf pkg-config libtool
 
 # Set up libreoffice config directory
 rm -Rf /var/libreoffice
@@ -26,5 +26,16 @@ mkdir -p /var/libreoffice/config
 /usr/lib/libreoffice/program/soffice.bin -env:UserInstallation=file:///var/libreoffice/config --headless --invisible --nocrashreport --nodefault --nofirststartwizard --nologo --norestore
 # Make sure sandstorm can write to config dir
 chown -R 1000:1000 /var/libreoffice
+
+# Compile capnp, for powerbox requests
+cd /tmp
+if [ ! -e capnproto ]; then git clone https://github.com/capnproto/capnproto; fi
+cd capnproto
+git checkout master
+cd c++
+autoreconf -i
+./configure
+make -j2
+sudo make install
 
 exit 0
